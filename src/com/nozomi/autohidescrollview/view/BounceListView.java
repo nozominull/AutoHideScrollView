5f40d7360a1d00e082dd09617ac659e9935451e5
Nozomi <xuyangbill@gmail.com>
2014-05-20 15:37:26 +0800

    remove useless code

--- src/com/nozomi/autohidescrollview/view/BounceListView.java
+++ src/com/nozomi/autohidescrollview/view/BounceListView.java
@@ -2,49 +2,27 @@ package com.nozomi.autohidescrollview.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.widget.ListView;
 
 public class BounceListView extends ListView {
-	private static final int MAX_Y_OVERSCROLL_DISTANCE = 200;
-
-	private Context mContext;
-	private int mMaxYOverscrollDistance;
 
 	public BounceListView(Context context) {
 		super(context);
-		mContext = context;
-		initBounceListView();
+
 	}
 
 	public BounceListView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mContext = context;
-		initBounceListView();
+
 	}
 
 	public BounceListView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		mContext = context;
-		initBounceListView();
-	}
 
-	private void initBounceListView() {
-		// get the density of the screen and do some maths with it on the max
-		// overscroll distance
-		// variable so that you get similar behaviors no matter what the screen
-		// size
-
-		final DisplayMetrics metrics = mContext.getResources()
-				.getDisplayMetrics();
-		final float density = metrics.density;
-
-		mMaxYOverscrollDistance = (int) (density * MAX_Y_OVERSCROLL_DISTANCE);
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		// TODO Auto-generated method stub
 		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
 				MeasureSpec.AT_MOST);
 
